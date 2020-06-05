d03ad07bcf668747a0fe5d04d82beb9f32ccdc9a
WilliamDevs <wlscott2011@gmail.com>
2020-06-04 19:30:17 -0500

    Started React Weather App

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './App.css';
+import WeatherCard from './WeatherCard';
+
+function App() {
+  return (
+    <div className="App">
+      <WeatherCard/>
+    </div>
+  );
+}
+
+export default App;
