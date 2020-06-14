9e3599b1370892589f777f3af4551a6978226d8b
WilliamDevs <wlscott2011@gmail.com>
2020-06-14 10:15:22 -0500

    OpenWeather Api Established

--- src/App.js
+++ src/App.js
@@ -4,6 +4,15 @@ import "./App.css";
 import WeatherCard from "./components/WeatherCard/WeatherCard";
 
 function App() {
+  const data = async () => {
+    const apiRes = await fetch(
+      "https://api.openweathermap.org/data/2.5/weather?q=London,uk&units=metric&appid=48ee2728a5c8d1781a1ea9984f428b0e"
+    );
+    const restJSON = await apiRes.json();
+    return restJSON;
+  };
+  console.log(data());
+  data().then((res) => console.log(res));
   return (
     <div className="App">
       <WeatherCard temp={30} condition="Clear" />
